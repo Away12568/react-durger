dd662f13bd3cdaf8c5afe082373938b11170a92d
awat <154649552+Away12568@users.noreply.github.com>
2024-10-19 19:58:19 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './app.css'
+
+const App = () => {
+  return (
+    <>
+    <div className="nav">
+      <div className="menu">
+      <a href="">home</a>
+      <a href="">about Us</a>
+      <a href="">contacts</a>
+      <a href="">sponsors</a>
+
+    
+      </div>
+
+<div className="burger">  <img className='burger' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDXByEOX4_AOWsnWR_Vhx8labuo3bsEy7P7g&s" alt="" /></div>
+      </div>
+
+    
+    </>
+  )
+}
+
+export default App
